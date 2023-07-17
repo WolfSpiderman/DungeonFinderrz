4b74848293e93c789616e5a450a993a7cd2368a2
blackshane <122633142+blackshane@users.noreply.github.com>
2023-07-17 18:51:51 -0500

    added request typeDef and defined resolver

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,20 +2,34 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql `
 type User {
-    _id: ID
-    userName: String
-    email: String
-    password: String
-    attendedGames: [Game]
+    _id: ID!
+    userName: String!
+    email: String!
+    password: String!
+    attendedGames: [Request]
     reliabilityRating: Float
 }
  
  type Game {
-    _id: ID
-    location: String
+    _id: ID!
+    location: String!
     description: String
-    totalPlayers: Int
+    totalPlayers: Int!
     players: Int
+ }
+
+ type Request {
+    _id: ID!
+    player: String!
+    role: String!
+    approved: Boolean
+    game:[Game]
+ }
+
+ type Query {
+    users: [User]
+    games: [Game]
+    requests:[Request]
  }
 `
 
