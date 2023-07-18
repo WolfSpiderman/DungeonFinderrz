a17f9d580d7beb364739ec6d76348e7ed0a73e4d
WolfSpiderman <wolfspiderman@yahoo.com>
2023-07-17 20:54:14 -0500

    model update

--- server/models/Game.js
+++ server/models/Game.js
@@ -1,28 +1,45 @@
 const { Schema, model } = require('mongoose');
 
-const gameSchema = new Schema({
-    location: {
-        type: String,
-        required: true
+const gameSchema = new Schema(
+    {
+        location: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+        },
+        totalPlayers: {
+            type: Number,
+            required: true,
+        },
+        players: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Request',
+                autopopulate: { select: 'name', match: { approved: true } },
+            },
+        ],
+        requests: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Request',
+                autopopulate: { select: 'name', match: { approved: null } },
+            },
+        ],
+        date: {
+            type: Date,
+            default: Date.now(),
+        },
     },
-    description: {
-        type: String
-    },
-    totalPlayers: {
-        type: Number,
-        required: true
-    },
-    players: {
-        type: Number,
-        default: 1
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
     }
-}, {
-    toJSON: { virtuals: true }, 
-    toObject: { virtuals: true } 
-});
+);
 
-gameSchema.virtual('slots').get(function() {
-    return this.totalPlayers - this.players;
+gameSchema.virtual('slots').get(function () {
+    return this.totalPlayers - this.players.length;
 });
 
 const Game = model('Game', gameSchema);
