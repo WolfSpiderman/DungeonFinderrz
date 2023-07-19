fe30e14e1cf08ea0716bb05a62a60f72ac06ec90
jbowdle <johnwbowdle@gmail.com>
2023-07-18 22:05:58 -0500

    fixed error

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,8 @@
 import { Link, redirect } from "react-router-dom";
 import "../styles/Home.css";
 import Auth from "../utils/auth";
+import Login from "../components/Login";
+import Signup from "../components/Signup";
 
 export async function loader({ request }) {
   const isLoggedIn = Auth.loggedIn();
@@ -17,7 +19,9 @@ export default function Home(props) {
   return (
     <div id="home-card">
       <div id="account-options">
+        <h2>Login</h2>
         <Login id="log-in"/>
+        <h2>Create Account</h2>
         <Signup id="create-account"/>
       </div>
       <div id="browse-link">
